c9c7d360166734ffa06f91cba76069b4ad49cf31
Mete Karasakal <mete@Metes-MacBook-Air.local>
2022-07-24 18:06:14 +0300

    transactions determine functions

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -49,3 +49,15 @@ func CoinbaseTx(to, data string) *Transaction {
 
 	return &tx
 }
+
+func (tx *Transaction) isCoinBase() bool {
+	return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
+}
+
+func (in *TxInput) CanUnlock(data string) bool {
+	return in.Sig == data
+}
+
+func (out *TxOutput) CanBeUnlocked(data string) bool {
+	return out.PubKey == data
+}
